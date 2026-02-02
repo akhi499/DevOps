c3d50f82d6a9bd406a5ec0fd2a146d5f2b53a249
akhi499 <yoman81382@gmail.com>
2026-02-02 20:00:58 +0530

    New demo

---
+++ App.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Maven build triggered successfully via Jenkins!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
